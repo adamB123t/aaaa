77d9bac249bf22c79f644bcfbdef3889bde62c5c
adamB123t <162359478+adamB123t@users.noreply.github.com>
2024-03-07 20:50:40 +0100

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,13 +1,13 @@
 @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "https://your-firebase-project.firebaseio.com",
-  projectId: "your-firebase-project",
-  storageBucket: "your-firebase-project.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBm7B83x4ZQB8OMQ7k_OgOUgU_Mhc7xXOY",
+  authDomain: "contatform-you.firebaseapp.com",
+  databaseURL: "https://contatform-you-default-rtdb.firebaseio.com",
+  projectId: "contatform-you",
+  storageBucket: "contatform-you.appspot.com",
+  messagingSenderId: "763714273380",
+  appId: "1:763714273380:web:f5d5032a4dbe2dc91d9f71"
 };
 
 
