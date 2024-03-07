cc0510bd4937620f01b72f9a6dfabcea353b079b
adamB123t <162359478+adamB123t@users.noreply.github.com>
2024-03-07 21:09:53 +0100

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,50 +1,50 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBm7B83x4ZQB8OMQ7k_OgOUgU_Mhc7xXOY",
-    authDomain: "contatform-you.firebaseapp.com",
-    databaseURL: "https://contatform-you-default-rtdb.firebaseio.com",
-    projectId: "contatform-you",
-    storageBucket: "contatform-you.appspot.com",
-    messagingSenderId: "763714273380",
-    appId: "1:763714273380:web:f5d5032a4dbe2dc91d9f71"
+  apiKey: "AIzaSyBm7B83x4ZQB8OMQ7k_OgOUgU_Mhc7xXOY",
+  authDomain: "contatform-you.firebaseapp.com",
+  databaseURL: "https://contatform-you-default-rtdb.firebaseio.com",
+  projectId: "contatform-you",
+  storageBucket: "contatform-you.appspot.com",
+  messagingSenderId: "763714273380",
+  appId: "1:763714273380:web:f5d5032a4dbe2dc91d9f71"
 };
 
-// Initialize firebase
+// initialize firebase
 firebase.initializeApp(firebaseConfig);
 
-// Reference your database
+// reference your database
 var contactFormDB = firebase.database().ref("cccc");
 
-document.getElementById("loginBtn").addEventListener("click", submitForm);
+document.getElementById("contactForm").addEventListener("submit", submitForm);
 
 function submitForm(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    var name = getElementVal("name");
-    var emailid = getElementVal("emailid");
+  var name = getElementVal("name");
+  var emailid = getElementVal("emailid");
 
-    saveMessages(name, emailid);
+  saveMessages(name, emailid);
 
-    // Enable alert
-    document.querySelector(".alert").style.display = "block";
+  //   enable alert
+  document.querySelector(".alert").style.display = "block";
 
-    // Remove the alert
-    setTimeout(() => {
-        document.querySelector(".alert").style.display = "none";
-    }, 3000);
+  //   remove the alert
+  setTimeout(() => {
+    document.querySelector(".alert").style.display = "none";
+  }, 3000);
 
-    // Reset the form
-    document.getElementById("contactForm").reset();
+  //   reset the form
+  document.getElementById("cccc").reset();
 }
 
 const saveMessages = (name, emailid) => {
-    var newContactForm = contactFormDB.push();
+  var newContactForm = contactFormDB.push();
 
-    newContactForm.set({
-        name: name,
-        emailid: emailid,
-    });
+  newContactForm.set({
+    name: name,
+    emailid: emailid,
+  });
 };
 
 const getElementVal = (id) => {
-    return document.getElementById(id).value;
+  return document.getElementById(id).value;
 };
